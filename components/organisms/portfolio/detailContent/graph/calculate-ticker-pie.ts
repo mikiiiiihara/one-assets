ae0a5bd3ecb9ab2d591c03381431eea7caab3691
井原 樹/Miki Ihara <mikiwhigh1274@gmail.com>
2024-05-13 02:05:36 +0900

    リファクタ

--- components/organisms/portfolio/detailContent/graph/calculate-ticker-pie.ts
+++ components/organisms/portfolio/detailContent/graph/calculate-ticker-pie.ts
@@ -2,7 +2,7 @@ import { PieData } from "@components/atoms/graph/pie";
 import { Detail } from "../../types";
 
 export const calculateTickerPie = (details: Detail[]): PieData[] => {
-  let graphData: PieData[] = new Array();
+  let graphValues: PieData[] = new Array();
   // 比率計算
   for (let data of details) {
     const yData = data.sumOfPrice;
@@ -10,9 +10,9 @@ export const calculateTickerPie = (details: Detail[]): PieData[] => {
       name: truncateString(data.name),
       y: Math.round(yData * 10) / 10,
     };
-    graphData.push(value);
+    graphValues.push(value);
   }
-  return graphData.sort(function (a, b) {
+  return graphValues.sort(function (a, b) {
     if (a.y > b.y) return -1;
     if (a.y < b.y) return 1;
     return 0;
