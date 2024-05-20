ac7132073c402e47597fbf62d3baa037be331d0b
井原 樹/Miki Ihara <mikiwhigh1274@gmail.com>
2024-05-20 09:22:55 +0900

    メニューバー作成

--- components/organisms/top/index.tsx
+++ components/organisms/top/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from "components/atoms/button";
+import { useRouter } from "next/router";
 import React from "react";
 
 export type Props = {
@@ -7,13 +8,25 @@ export type Props = {
 };
 
 const Top: React.FC<Props> = ({ name, signOut }) => {
+  const router = useRouter();
   return (
-    <>
+    <div className="text-center">
       <p className="font-bold">{name}でログイン中</p>
-      <Button className="bg-gray-500 text-white" onClick={() => signOut()}>
-        ログアウト
-      </Button>
-    </>
+      <div className="flex m-6 justify-center">
+        <Button className="text-white" onClick={() => router.push("/")}>
+          資産推移
+        </Button>
+        <Button
+          className="text-white"
+          onClick={() => router.push("/portfolio")}
+        >
+          ポートフォリオ
+        </Button>
+        <Button className="bg-gray-500 text-white" onClick={() => signOut()}>
+          ログアウト
+        </Button>
+      </div>
+    </div>
   );
 };
 
